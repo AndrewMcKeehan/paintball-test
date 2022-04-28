2580320238428f90981a2d3a075167a0fdf0eddb
andrew50-2069137 <72579703+andrew50-2069137@users.noreply.github.com>
2022-04-27 17:56:53 -0700

    Changes

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    float ammo = 30f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (ammo <= 0) { return; }
+        ammo -= 1;
+        
+
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            } else
+            {
+                // splat()
+            }
+        }
+
+    }
+}
